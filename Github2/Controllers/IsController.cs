1056f0d7ea21ea04aee8287cd1a17538db1974d1
Melis Akay <melisbegumakay@yahoo.com>
2023-07-11 20:57:21 +0300

    oluşturuldu

---
+++ Github2/Controllers/IsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Github2.Controllers
+{
+    public class IsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
